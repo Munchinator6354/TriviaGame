71c49b099f3de0d8d2b76b87d3d548eba663a925
Ryan Isaacson <munchinator6354@gmail.com>
2019-09-26 00:29:35 -0700

    added 10 questions with correct answers to html, added rudimentary pseudocoding to javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+// Make a function called startQuiz
+function startQuiz() {
+    $("#questionsArea").onclick("#startButton")
+        document.write("blah");
+        
+    }
+
+
+// Make function startQuiz get activited from the click of the #startButton button
+
+// Within startQuiz function have it grab the "#questionsArea" and overwrite what is there currently (the #startButton)
+
+// Make a timer that starts counting down for 120 seconds from the onclick button event.
+
+// Make the 120 second timer display text showing the timer count down at the top of the #questionsArea div.
+
+// Beneath the 120 second timer, display a set of 10 questions
+
+// Each question will have 4 possible answers (a, b, c, d). Make it so only 1 answer can be selected of the 4 at a time.
+
+// Each answer will have a "bubble" next to it to click and mark it as the selected answer
+
+// Make a button append beneath the 10 questions that says "Done"
+
+// Create an endQuiz function that overwrites the "#questionsArea" div with
+    //1) Amount of Correct Answers
+    //2) Amount of Incorrect Answers
+    //3) Amount of Questions Unanswered
+// Make the endQuiz function get called when the timer reaches 0 OR if the client clicks the "#doneButton"
+
+
+
+
+
+    
+
+    
