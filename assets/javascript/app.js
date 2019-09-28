204ef59eae42ce3a24a5db2d74ebe807ff1695bc
Ryan Isaacson <munchinator6354@gmail.com>
2019-09-27 21:48:22 -0700

    changes made after tutor session since last class, added code to make SOMETHING appear with button click using code I haven't seen before and don't quite understand yet.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,117 @@
-// Make a function called startQuiz that gets called when the #startButton is clicked
+// // Make a function called startQuiz that gets called when the #startButton is clicked
 
-var que1 = {"question": "Question #1 - How many members of the Istari are there?",
-            "answer": 2,
-            "options": ["a) 7","b) 4", "c) 5", "d) 16"]}
+// var que1 = {"question": "Question #1 - How many members of the Istari are there?",
+//             "answer": 2,
+//             "options": ["a) 7","b) 4", "c) 5", "d) 16"]}
+
+// var que2 = {"question": "what is ",
+//             "answer": 1,
+//             "options": ["answer","answer2", "answer3", "answer4"]}
+
+// var que3 = {"question": "what is ",
+//             "answer": 1,
+//             "options": ["answer","answer2", "answer3", "answer4"]}
+
+// var que4 = {"question": "what is ",
+//             "answer": 1,
+//             "options": ["answer","answer2", "answer3", "answer4"]}
+
+
+// var que5 = {"question": "what is ",
+//             "answer": 1,
+//             "options": ["answer","answer2", "answer3", "answer4"]}
+
+// var que6 = {"question": "what is ",
+//             "answer": 1,
+//             "options": ["answer","answer2", "answer3", "answer4"]}
+
+// var que7 = {"question": "what is ",
+//             "answer": 1,
+//             "options": ["answer","answer2", "answer3", "answer4"]}
+
+// var que8 = {"question": "what is ",
+//             "answer": 1,
+//             "options": ["answer","answer2", "answer3", "answer4"]}
+
+// var que9 = {"question": "what is ",
+//             "answer": 1,
+//             "options": ["answer","answer2", "answer3", "answer4"]}
+
+// var que10 = {"question": "what is ",
+//             "answer": 1,
+//             "options": ["answer","answer2", "answer3", "answer4"]}            
+// var arrQue = [que1,que2,que3,que4,que5,que6,que7,que8,que9,que10]
+// for (var i = 0; i < arrQue.length; i++) {
+//   $("#questionsArea").append(i);
+// }
+
+
+var questions = [
+
+    // ques1 is also the object but is your question you are going to display to the page,
+    // correct answer is questions.ques1.correct
+
+    // how to display the question
+    // $('#display').html(questions.ques1.question)
+
+    {
+        question: 'what is your name',
+        choices: ['tom', 'andy', 'john'],
+        correct: "andy"
+    },
+
+    {
+        question: 'what is your age',
+        choices: [20, 30, 40],
+        correct: 30
+    },
+
+    {
+        question: 'what is your favorite food',
+        choices: ['apple', 'pear', 'banana'],
+        correct: "pear"
+    }
+
+
+
+
+]
+
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 10,
+    start: function () {
+        for (i = 0; i < questions.length; i++) {
+            $("#questionsArea").append(`<p>${questions[i].question}</p>`);
+        for (j = 0; j < questions[i].choices.length; j++) {
+            $("#questionsArea").append(`<input type="radio" name="question-"${i} value="${questions[i].choices[j]}>"`)
+        }
             
-var que2 = {"question": "what is ",
-            "answer": 1,
-            "options": ["answer","answer2", "answer3", "answer4"]}
-
-var que3 = {"question": "what is ",
-            "answer": 1,
-            "options": ["answer","answer2", "answer3", "answer4"]}
-
-var que4 = {"question": "what is ",
-            "answer": 1,
-            "options": ["answer","answer2", "answer3", "answer4"]}
-            
-var que5 = {"question": "what is ",
-            "answer": 1,
-            "options": ["answer","answer2", "answer3", "answer4"]}
-
-var que6 = {"question": "what is ",
-            "answer": 1,
-            "options": ["answer","answer2", "answer3", "answer4"]}
-
-var que7 = {"question": "what is ",
-            "answer": 1,
-            "options": ["answer","answer2", "answer3", "answer4"]}
-
-var que8 = {"question": "what is ",
-            "answer": 1,
-            "options": ["answer","answer2", "answer3", "answer4"]}
-            
-var que9 = {"question": "what is ",
-            "answer": 1,
-            "options": ["answer","answer2", "answer3", "answer4"]}
-
-var que10 = {"question": "what is ",
-            "answer": 1,
-            "options": ["answer","answer2", "answer3", "answer4"]}            
-
-
-var arrQue = [que1,que2,que3,que4,que5,que6,que7,que8,que9,que10]
-
-
-for (var i = 0; i < arrQue.length; i++) {
-  $("#questionsArea").append(i);
+        }
+    }
 }
 
 
 
-//$("#startButton").click(function(){
-    //.html("The paragraph was clicked.");
-//});
+$("#startButton").click(function () {
+    game.start();
+});
+
+
+//make countdown method to start timer (it will append the timer to the html, also will start timer)
+//also make condition method that checks the counter, and if it is === 0 will invoke the "done" function
+//done function will stop timer and check user input for the choices
+//append amount of correct and incorrect answers
+//if users choice === the game.correct 0 1 or 2 which ever is right, increment the "correct" property by ++
+//otherwise increment incorrect property ++
+//append correct and incorrect answers to the html
+
+
+
+
+
+
 
 
 
@@ -77,11 +136,3 @@ for (var i = 0; i < arrQue.length; i++) {
     //2) Amount of Incorrect Answers
     //3) Amount of Questions Unanswered
 // Make the endQuiz function get called when the timer reaches 0 OR if the client clicks the "#doneButton"
-
-
-
-
-
-    
-
-    
